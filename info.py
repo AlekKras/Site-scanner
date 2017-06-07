f4c697fa7036f1f7e49558ad748f1fb24c580a9b
Aleksandr <sasha.krasnov@list.ru>
2017-06-07 22:43:32 +0300

    Analogy of Linux whois program

---
+++ info.py
@@ -0,0 +1,10 @@
+import os
+
+#analogy of Linux whois command
+def get_info(url):
+    command = "whois" + url
+    process = os.popen(command)
+    results = str(process.read())
+    return results
+
+print(get_info("reddit.com"))
